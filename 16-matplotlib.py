47081f91d2e3adf06017368614e6ae2e397920ce
Ansuman <ansuman.mishra1@ge.com>
2017-09-11 19:21:01 +0530

    Adding examples for pie and line chart

--- 16-matplotlib.py
+++ 16-matplotlib.py
@@ -5,5 +5,22 @@ import numpy as np
 x = [1, 2, 3, 4, 5]
 y = [4, 5, 5, 7 , 8]
 
+plt.xlabel('Speed')
+plt.ylabel('Hr')
+
 plt.plot(x, y)
+plt.show()
+
+''' Drawing a Pie Chart '''
+
+# Data to plot
+labels = 'Python', 'C++', 'Ruby', 'Java'
+sizes = [215, 130, 245, 210]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0, 0, 0)  # explode 1st slice
+
+# Plot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
 plt.show()
---
+++ 17-pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Creating a data frame
+df = pd.DataFrame({
+		"a": [1, 2, 3],
+		"b": [5, 6, 7]
+	})
+
+print(df)
